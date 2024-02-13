c5b6c9c77d6ca118102dd776965bb6eb043fc8e5
Anand Chaniyara <155147373+anand-chaniyara@users.noreply.github.com>
2024-02-13 22:28:56 +0530

    Update practical_1_B.py

--- practical_1_B.py
+++ practical_1_B.py
@@ -2,6 +2,6 @@ p = float(input("Enter the Principal amount: "))
 r = float(input("Enter the Rate of interest: "))
 t = float(input("Enter the Time period in years: "))
 
-si = (p * r * t) / 100
+simple_interest = (p * r * t) / 100
 
-print("The simple interest is ", si)
+print("The simple interest is ", simple_interest)
